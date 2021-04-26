401d2ac05d77378446a252da713521a559cc1a7b
unknown <sobolak>
2021-04-26 14:15:17 +0200

    cybant

--- Workout/filters.py
+++ Workout/filters.py
@@ -1,13 +1,16 @@
 import django_filters
+from django_filters import CharFilter
 
 from .models import *
 
 class FriendFilter(django_filters.FilterSet):
+    username = CharFilter(field_name = 'username' , lookup_expr = 'icontains')
     class Meta:
         model = Customer
         fields = ['username']
 
 class TextFilter(django_filters.FilterSet):
+    text = CharFilter(field_name = 'text' , lookup_expr = 'icontains')
     class Meta:
         model = Dialogues
         fields = ['text']
