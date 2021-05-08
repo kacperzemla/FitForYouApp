102a07ad485e2c451d1b9612d2d1db9080396a50
unknown <sobolak>
2021-05-08 23:02:20 +0200

    finisz

--- FitForYou/settings.py
+++ FitForYou/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
@@ -126,3 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
